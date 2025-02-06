54b0dca4bc46759062712aa139c3d5bc632d995d
niiiihal <nihagni18@gmail.com>
2025-02-06 06:30:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"  // treat all js code as newer version  
+let name = "mihal"
+let age = 23
+let isloggedin = false
+
+// datatypes
+
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol => unqiue
+
+//object
+
+console.log(typeof null) // ojecct
+console.log(typeof undefined) // undefined
