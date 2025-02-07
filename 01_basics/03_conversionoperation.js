66bf9b694f97dbc95d45f1a75713c44497b363b1
niiiihal <nihagni18@gmail.com>
2025-02-07 05:28:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+/*
+let score="33"
+console.log(typeof score)
+let valueinteger=Number(score)
+console.log( typeof valueinteger)
+*/
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false =>0
+// 
+let score=null
+console.log(typeof score)
+let valueinteger=Number(score)
+console.log(typeof valueinteger)
+console.log(valueinteger)
+
+// 1=> true ; 0=> false
+//""=>false
+//"nihal"=true
+
+
+
