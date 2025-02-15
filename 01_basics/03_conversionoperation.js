725e8071625e0fa51dcbcd0c7361e1a5cf2e1969
niiiihal <nihagni18@gmail.com>
2025-02-15 12:01:54 +0000

    comparision check

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -9,14 +9,26 @@ console.log( typeof valueinteger)
 // true => 1 ; false =>0
 // 
 let score=null
-console.log(typeof score)
+//console.log(typeof score)
 let valueinteger=Number(score)
-console.log(typeof valueinteger)
-console.log(valueinteger)
+//console.log(typeof valueinteger)
+//console.log(valueinteger)
 
 // 1=> true ; 0=> false
 //""=>false
 //"nihal"=true
+
+// console.log("2">1)
+// console.log(null>0)
+console.log(null==0);
+// console.log(null<0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+
+console.log("2"===2); // because datatyes are diffenrt
+
+
 
 
 
