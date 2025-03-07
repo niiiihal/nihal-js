99c271b8bb563d4da1d337654850d8809974ab1a
niiiihal <nihagni18@gmail.com>
2025-03-07 09:19:44 +0000

    introducing maths

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,41 @@
+const score=400;
+// console.log(score);
+
+const bal= new Number(300);
+// console.log(bal);
+
+//  console.log(bal.toString().length)
+// console.log(typeof(bal))
+// console.log(bal.toFixed(2))
+
+const othernum= 123.699
+// console.log(othernum.toPrecision(4))
+
+const num=10000000;
+console.log(num.toLocaleString('en-IN')); // CONVERTS NO INTO INDIAN FORMAT
+
+// +++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-6))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.65));
+// console.log(Math.floor(5.25));
+// console.log(Math.min(4,5,56,8,2,4));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
+
+
+
+
+
+
